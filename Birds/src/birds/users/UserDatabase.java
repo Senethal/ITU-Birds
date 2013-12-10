15515341a81b2311cdcca1e981b07dddede195a3
Timm8 <tomas.lascak@gmail.com>
2013-12-10 12:10:08 +0100

    Databaze userů

--- Birds/src/birds/users/UserDatabase.java
+++ Birds/src/birds/users/UserDatabase.java
@@ -12,6 +12,10 @@ import javax.xml.parsers.*;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.*;
 import org.xml.sax.SAXException;
@@ -174,16 +178,14 @@ public class UserDatabase {
     } 
     
     
-    public static int AddTest(int proc, String Name) throws ParserConfigurationException, SAXException, TransformerConfigurationException, IOException, TransformerException {
+    public static int AddTest(int proc, String Name, int less, int type) throws ParserConfigurationException, SAXException, TransformerConfigurationException, IOException, TransformerException {
         DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance(); 
         domFactory.setIgnoringComments(true);
         DocumentBuilder builder = domFactory.newDocumentBuilder(); 
         Document doc = builder.parse(new File("examples//databaze.xml"));
         int max = 0;
         int celkem = 0;
-        
-        
-        
+
         NodeList nUser = doc.getElementsByTagName("User");
         
         for (int temp2 = 0; temp2 < nUser.getLength(); temp2++) {
@@ -206,11 +208,10 @@ public class UserDatabase {
                              if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                                 
                                 Element element = (Element) nNode;
-                                celkem += Integer.parseInt(element.getTextContent());
-                                if (Integer.toString(max).equals(element.getAttribute("Test"))) {
+                                celkem += Integer.parseInt(element.getTextContent().substring(1,3));
+                                if (Integer.toString(max).equals(element.getTextContent().substring(0,1))) {
                                     max++;
                                     celkem = (celkem)/(max+1); 
-                       
                                 }   
                              }            
                         }        
@@ -221,8 +222,14 @@ public class UserDatabase {
                     eElement2.appendChild(Max); 
                         
                     Element pom = doc.createElement("Testik");
-                    pom.appendChild(doc.createTextNode(Integer.toString(proc)));
-                    pom.setAttribute("Test", Integer.toString(max));
+                    String text;
+                    
+                    if ( proc == 0 )
+                        text = Integer.toString(max) + Integer.toString(proc) + "0" + Integer.toString(less) + Integer.toString(type);
+                    else
+                        text = Integer.toString(max) + Integer.toString(proc) + Integer.toString(less) + Integer.toString(type);
+                    
+                    pom.appendChild(doc.createTextNode(text));
                     eElement2.appendChild(pom);                  
                 }
                     
@@ -239,4 +246,131 @@ public class UserDatabase {
         return celkem;
     }
    
+    public static String TypLess(int ID, String Name) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance(); 
+        domFactory.setIgnoringComments(true);
+        DocumentBuilder builder = domFactory.newDocumentBuilder(); 
+        Document doc = builder.parse(new File("examples//databaze.xml"));  
+        NodeList nUser = doc.getElementsByTagName("User");
+        
+        for (int temp2 = 0; temp2 < nUser.getLength(); temp2++) {
+            Node nNode2 = nUser.item(temp2);
+            if (nNode2.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement2 = (Element) nNode2;          
+            
+                if (Name == null ? eElement2.getElementsByTagName("Jmeno").item(0).getTextContent() == null : Name.equals(eElement2.getElementsByTagName("Jmeno").item(0).getTextContent())) {
+                    NodeList nTest = eElement2.getElementsByTagName("Testik");
+                    
+                    for (int temp = 0; temp < nTest.getLength(); temp++) {
+                        if (Integer.toString(ID).equals(eElement2.getElementsByTagName("Testik").item(temp).getTextContent().substring(0,1))) { 
+                            return eElement2.getElementsByTagName("Testik").item(temp).getTextContent().substring(3, 4);
+                        }
+                    }  
+                }
+            }
+        } 
+        return null;
+    }
+    
+    public static String TypTest(int ID, String Name) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance(); 
+        domFactory.setIgnoringComments(true);
+        DocumentBuilder builder = domFactory.newDocumentBuilder(); 
+        Document doc = builder.parse(new File("examples//databaze.xml"));
+        NodeList nUser = doc.getElementsByTagName("User");
+        
+        for (int temp2 = 0; temp2 < nUser.getLength(); temp2++) {
+            Node nNode2 = nUser.item(temp2);
+            if (nNode2.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement2 = (Element) nNode2;          
+            
+                if (Name == null ? eElement2.getElementsByTagName("Jmeno").item(0).getTextContent() == null : Name.equals(eElement2.getElementsByTagName("Jmeno").item(0).getTextContent())) {
+                    NodeList nTest = eElement2.getElementsByTagName("Testik");
+                    
+                    for (int temp = 0; temp < nTest.getLength(); temp++) {
+                        Node nNode1 = nUser.item(temp2);
+                        if (nNode1.getNodeType() == Node.ELEMENT_NODE) {  
+                            Element eElement1 = (Element) nNode1;
+                            if (Integer.toString(ID).equals(eElement1.getElementsByTagName("Testik").item(temp).getTextContent().substring(0,1))) { 
+                                return eElement1.getElementsByTagName("Testik").item(temp).getTextContent().substring(4, 5);
+                             } 
+                        }    
+                    }
+                
+                }
+            }
+        }
+        return "0";
+    }
+    
+    public static String GetResult(int ID, String Name) throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance(); 
+        domFactory.setIgnoringComments(true);
+        DocumentBuilder builder = domFactory.newDocumentBuilder(); 
+        Document doc = builder.parse(new File("examples//databaze.xml"));
+        NodeList nUser = doc.getElementsByTagName("User");
+        
+        for (int temp2 = 0; temp2 < nUser.getLength(); temp2++) {
+            Node nNode2 = nUser.item(temp2);
+            if (nNode2.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement2 = (Element) nNode2;          
+            
+                if (Name == null ? eElement2.getElementsByTagName("Jmeno").item(0).getTextContent() == null : Name.equals(eElement2.getElementsByTagName("Jmeno").item(0).getTextContent())) {
+                    NodeList nTest = eElement2.getElementsByTagName("Testik");
+                    
+                    for (int temp = 0; temp < nTest.getLength(); temp++) {
+                        Node nNode1 = nUser.item(temp2);
+                        if (nNode1.getNodeType() == Node.ELEMENT_NODE) {  
+                            if (Integer.toString(ID).equals(eElement2.getElementsByTagName("Testik").item(temp).getTextContent().substring(0,1))) { 
+                                return eElement2.getElementsByTagName("Testik").item(temp).getTextContent().substring(1, 3);
+                             } 
+                        }    
+                    }
+                
+                }
+            }
+        }
+        return "0";
+    }  
+    
+    public static int GetMax(String Name) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance(); 
+        domFactory.setIgnoringComments(true);
+        DocumentBuilder builder = domFactory.newDocumentBuilder(); 
+        Document doc = builder.parse(new File("examples//databaze.xml"));
+        NodeList nUser = doc.getElementsByTagName("User");
+        
+        for (int temp2 = 0; temp2 < nUser.getLength(); temp2++) {
+            Node nNode2 = nUser.item(temp2);
+            if (nNode2.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement2 = (Element) nNode2;          
+            
+                if (Name == null ? eElement2.getElementsByTagName("Jmeno").item(0).getTextContent() == null : Name.equals(eElement2.getElementsByTagName("Jmeno").item(0).getTextContent())) {
+                    return Integer.parseInt(eElement2.getElementsByTagName("Maximum").item(0).getTextContent());
+                }
+            }
+        }
+        return 0;
+    }
+    
+    public static String GetVysledek(String Name) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory domFactory = DocumentBuilderFactory.newInstance(); 
+        domFactory.setIgnoringComments(true);
+        DocumentBuilder builder = domFactory.newDocumentBuilder(); 
+        Document doc = builder.parse(new File("examples//databaze.xml"));
+        NodeList nUser = doc.getElementsByTagName("User");
+        
+        for (int temp2 = 0; temp2 < nUser.getLength(); temp2++) {
+            Node nNode2 = nUser.item(temp2);
+            if (nNode2.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement2 = (Element) nNode2;          
+            
+                if (Name == null ? eElement2.getElementsByTagName("Jmeno").item(0).getTextContent() == null : Name.equals(eElement2.getElementsByTagName("Jmeno").item(0).getTextContent())) {
+                    return eElement2.getElementsByTagName("Vysledek").item(0).getTextContent();
+                }
+            }
+        }
+        return "0";
+    }
+    
 }
